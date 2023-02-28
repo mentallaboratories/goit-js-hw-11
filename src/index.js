795c1777ad308480180f40b288da37f584a9657c
mentallaboratories <mentallaboratories@gmail.com>
2023-03-01 01:17:41 +0200

    get render page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,51 @@
 import axios from 'axios';
 
-// Make a request for a user with a given ID
-axios.get('https://pixabay.com/api/?key=33947386-52d504ac84a1d8cdc1096e7de&q=yellow+flowers&image_type=photo&pretty=true')
-  .then(function (response) {
-    // handle success
-    console.log(response);
+const refs = {
+  form : document.querySelector('#search-form'),
+  gallery : document.querySelector('.gallery')
+};
+let searchQuery ='';
+const pageNum ="1";
+
+refs.form.addEventListener('submit', onSubmit);
+
+
+
+
+function onSubmit(e){
+  e.preventDefault();
+  searchQuery = e.currentTarget.elements.searchQuery.value;
+  console.log(searchQuery)
+
+axios.get(`https://pixabay.com/api/?key=33947386-52d504ac84a1d8cdc1096e7de&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${pageNum}&per_page=40`)
+  .then((response) => {return response.data})
+  .then((data) => {
+    let markupItem = '';
+    let markup = '';
+// markup cycle
+    for(const hit of data.hits){
+      markupItem = (`
+      <div class="photo-card">
+        <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes: </b>${hit.likes}
+          </p>
+          <p class="info-item">
+            <b>Veiws: </b>${hit.views}
+          </p>
+          <p class="info-item">
+            <b>Comments: </b>${hit.comments}
+          </p>
+          <p class="info-item">
+            <b>Downloads: </b>${hit.downloads}
+          </p>
+        </div>
+      </div>
+                  `);
+      markup += markupItem;
+    }
+    refs.gallery.innerHTML = markup;
   })
+}
+
